401ed056ac227cf28ebcd08684c04f5bdc620b97
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-16 23:08:17 -0500

    api changes

--- blog_api/views.py
+++ blog_api/views.py
@@ -5,7 +5,10 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
-    pass
+
 
 class PostDetail(generics.RetrieveDestroyAPIView):
-    pass
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+#Read on Views to read only
