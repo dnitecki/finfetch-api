762b0e85f5a4672d715877f408870ee523ebbf92
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-07 23:25:52 -0500

    settings changes

--- core/settings.py
+++ core/settings.py
@@ -138,9 +138,36 @@ REST_FRAMEWORK ={
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+ALLOWED_HOSTS=['*']
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000"
 # ]
+# CORS_ORIGIN_WHITELIST = (
+# "http://localhost:3000"
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
