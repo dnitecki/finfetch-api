f943f0ae884defc3d8ad2e905d2cb9d396ea44d7
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-23 20:10:25 -0500

    remove

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('blog.urls', namespace='blog')),
-    # path('api/', include('blog_api.urls', namespace='blog_api')),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('stocks.urls', namespace='stocks')),
     path('api/', include('user.urls', namespace='user'))
 
