79ad5e7be586acb58eca9dba4beef7b1a1118c4f
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-06 18:25:37 -0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 REST_FRAMEWORK ={
 'DEFAULT_PERMISSION_CLASSES': [
