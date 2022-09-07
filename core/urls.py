b86d82391bb8c6114b5ed263afdef0812f71536f
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-07 09:40:56 -0500

    change url admin

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('stocks.urls', namespace='stocks')),
     path('api/', include('user.urls', namespace='user'))
 
